22bd2fe4131af9ceafdd767a9a2f6f1186394cc9
Rhett Chase <rhettchase@yahoo.com>
2023-04-04 11:42:51 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+const names = []
+
+function writeCards(names, event) {
+    const messages = [];
+    for (let i = 0; i < names.length; i++) {
+        const message = `Thank you, ${names[i]}, for the wonderful ${event} gift!`;
+        messages.push(message);
+    }
+   return messages;
+}
+
+function countDown() {
+    let count = 10;
+    while (count >= 0) {
+        console.log(count--);
+    }
+}
